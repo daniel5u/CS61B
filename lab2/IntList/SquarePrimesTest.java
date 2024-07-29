f347fb69b970b6d261734177a5312855bd9a3881
DanielSu <danielsuuuuuu@gmail.com>
2024-07-29 14:21:49 +0800

    lab2 completed

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple0() {
+        IntList lst = IntList.of(2, 3, 11, 10, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 121 -> 10 -> 18", lst.toString());
+        assertTrue(changed);
+    }
 }
